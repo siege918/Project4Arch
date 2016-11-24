45d78da7128898ca02f21addd76e54602e7c64bb
Aaron <ascherer1993@gmail.com>
2016-11-23 17:24:36 -0800

    Adding HeaderFile

---
+++ Scoreboard.h
@@ -0,0 +1,39 @@
+#ifndef SCOREBOARD_H_INCLUDED
+#define SCOREBOARD_H_INCLUDED
+
+enum FU {INTEGER, MULT1, MULT2, ADD, DIVIDE};
+enum OP {TEST};
+
+struct Instruction_Status {
+    int Issue;
+    int Read_Op;
+    int Exec_Complete;
+    int Write_Result;
+};
+
+//struct Instruction_Status Instruction_Statuses[];
+
+struct FU_Status {
+    int busy;
+    enum OP op_code;
+    int Fi;
+    int Fj;
+    int Fk;
+    enum FU Qj;
+    enum FU Qk;
+    int Rj;
+    int Rk;
+};
+
+// struct FU_Status FU_Statuses[];
+
+// struct FU Register_Result_Status[];
+
+
+struct ScoreBoard{
+    struct FU_Status FU_Statuses[5];
+    struct Instruction_Status Instruction_Statuses[10];
+    enum FU Register_Result_Status[32];
+};
+
+#endif
